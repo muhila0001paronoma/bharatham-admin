eeaedb7f30f94d6ec8ef13cb2aed1a273a262314
muhila0001paronoma <muhila0001paronoma@gmail.com>
2025-12-17 09:38:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-950">
+      <h1 className="text-white text-2xl font-bold">Bharatham Admin Panel</h1>
+    </div>
+  )
+}
+
+export default App
